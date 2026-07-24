be3e88bbf831f3529a8883a21dd181c4d4057186
aiken214 <paul.arsolon@deped.gov.ph>
2026-07-24 12:50:13 +0800

    minor fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="theme-color" content="#4338ca">
+    <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <link rel="manifest" href="{{ asset('manifest.json') }}">
@@ -323,7 +324,7 @@
         }
 
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('{{ asset("sw.js") }}')
+            navigator.serviceWorker.register('/sw.js')
                 .then(function(reg) { reg.update(); });
         }
     </script>
